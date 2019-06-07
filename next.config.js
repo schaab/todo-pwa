74a762dfc77e10a6aeae42bf139a0c9c6608c443
Jared Schaab <jared.schaab@gmail.com>
2019-06-06 20:31:29 -0700

    chore: config pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
-const withOffline = require('next-offline')
+const withOffline = require("next-offline");
 
 module.exports = withOffline({
-    target: 'serverless'
-  })
+  target: "serverless",
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+});
