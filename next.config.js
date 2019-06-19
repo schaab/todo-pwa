831bd9ed865afeaafdc3a11eaf6d9d8148349cb8
Jared Schaab <jared.schaab@gmail.com>
2019-06-19 14:53:07 -0700

    feat: Make a PWA (#2)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,27 @@
-module.exports = {
-    target: 'serverless'
-  }
+const withOffline = require('next-offline')
+
+const config = {
+  target: 'serverless',
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+}
+
+module.exports = withOffline(config)
--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import App, { Container } from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+import React from 'react'
+import App, {Container} from 'next/app'
+import Head from 'next/head'
+import {ThemeProvider} from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '../src/theme'
 
 class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
-      jssStyles.parentNode.removeChild(jssStyles);
+      jssStyles.parentNode.removeChild(jssStyles)
     }
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const {Component, pageProps} = this.props
 
     return (
       <Container>
         <Head>
-          <title>My page</title>
+          <title>Let's do this</title>
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
@@ -28,8 +28,8 @@ class MyApp extends App {
           <Component {...pageProps} />
         </ThemeProvider>
       </Container>
-    );
+    )
   }
 }
 
-export default MyApp;
+export default MyApp
